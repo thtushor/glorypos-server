9fae7bab1bd4358d1c8fbdc84188d37112d7df0d
Sohidul-Islam <sishufol.sim@gmail.com>
2025-11-21 01:04:44 +0600

    change route

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -46,9 +46,7 @@ router.get('/users', AuthService.authenticate, requestHandler(null, async (req, 
 }));
 
 router.get('/sub-shops', AuthService.authenticate, addShopAccess, requestHandler(null, async (req, res) => {
-    const userId = req?.query?.userId||req?.user?.id;
-
-    console.log({req: req})
+    const userId = req?.query?.userId || req?.user?.id;
 
     if (!userId) {
         return res.status(400).json({ status: false, message: "userId is required", data: null });
